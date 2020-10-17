735999c988970358f351bac808c2e98ff4022415
Thai Thien <tthien@apcs.vn>
2020-10-17 17:10:14 +0700

    send url

--- get-current-url-simple/main.js
+++ get-current-url-simple/main.js
@@ -1,3 +1,10 @@
 console.log(window.location.href);
 console.log("window.location.href = " + window.location.href);
 console.log("main.js MEOWWWW this is a log of content script");
+
+var data = {"timestamp": Math.floor(Date.now() / 1000),"url":window.location.href};
+var yourUrl= "https://requestbin.io/1i5gjg51";
+var xhr = new XMLHttpRequest();
+xhr.open("POST", yourUrl, true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify(data));
