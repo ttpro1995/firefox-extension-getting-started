ddf4f62729313c0a1163272f906467ac09294b4a
Thai Thien <tthien@apcs.vn>
2020-10-17 16:50:44 +0700

    get current url

---
+++ get-current-url-simple/README.md
@@ -0,0 +1,3 @@
+Icon attribute
+
+![pen.svg](icons/pen.svg) Icons made by [Freepik](https://www.flaticon.com/authors/freepik) from [Flaticon](www.flaticon.com)
---
+++ get-current-url-simple/main.js
@@ -0,0 +1,3 @@
+console.log(window.location.href);
+console.log("window.location.href = " + window.location.href);
+console.log("main.js MEOWWWW this is a log of content script");
